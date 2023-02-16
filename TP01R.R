155958e06f0ac1ca3f4ebf74076557517be32e23
williamh3 <119340331+williamh3@users.noreply.github.com>
2023-02-16 08:01:39 -0500

    Add files via upload

---
+++ TP01R.R
@@ -0,0 +1,40 @@
+rm(list=ls())
+library(ISLR2)
+library(splines)
+library(gam)
+data('Wage')
+attach(Wage)
+colnames(Wage)
+# maritl, jobclass, health_ins
+Wage <- Wage[c('wage','maritl','jobclass','health_ins','health')]
+summary(Wage)
+
+train <- sample(1:nrow(Wage),nrow(Wage)*.8)
+test <- (-train)
+par(mfrow=c(2,2))
+
+gam1 <- gam(wage~.,data=Wage[train,])
+summary(gam1)
+coef(gam1)
+preds1 <- predict(gam1,newdata=Wage)
+gam1.mse <- mean((preds1-Wage$wage[test])^2)
+plot(gam1,se=T)
+
+gam2 <- gam(I(wage>250)~.,data=Wage[train,])
+summary(gam2)
+coef(gam2)
+preds2 <- predict(gam2,newdata=Wage)
+gam2.mse <- mean((preds2-Wage$wage[test])^2)
+table(jobclass,I(wage>250))
+table(maritl,I(wage>250))
+table(health_ins,I(wage>250))
+table(health,I(wage>250))
+plot(gam2,se=T)
+
+gam3 <- gam(I(wage<85.38)~.,data=Wage[train,])
+summary(gam3)
+coef(gam3)
+preds3 <- predict(gam3,newdata=Wage)
+gam3.mse <- mean((preds3-Wage$wage[test])^2)
+
+write.csv(Wage,'C:/Users/William H/Desktop/Spring_23/ML2/DataSets/wage.csv')
